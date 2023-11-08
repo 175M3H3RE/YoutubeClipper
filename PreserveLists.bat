e82322ff98521e602f3ceccb78e987e86422ad24
175M3H3RE <117502862+175M3H3RE@users.noreply.github.com>
2023-11-08 11:41:01 +0530

    Add files via upload

--- PreserveLists.bat
+++ PreserveLists.bat
@@ -1,5 +1,7 @@
 @echo on
 for /f "tokens=*" %%i in ('powershell -c "(Get-clipboard)"') do set string="%%i"
+echo %string%|find /v /i "youtube.com"&&echo %string%>Url.old.txt
+echo %string%|find /v /i "youtube.com"&&(echo.Error: Invalid Url>>Url.old.txt&echo. Make Sure U copy a Valid Url>>Url.old.txt&exit)
 for /f "tokens=*" %%i in (%string%) do echo %%i>Url.old.txt
 echo.String is %string%
 for /f "tokens=5 delims=.:?/^=&" %%i in (%string%) do set watchis=%%i
